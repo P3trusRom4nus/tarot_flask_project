a93549a8836eb95e5831be01481002e053431213
“P3trusRom4nus” <triple9.ar@gmail.com>
2024-10-06 17:02:48 -0300

    spinner loader added to everywhere

--- static/js/booking.js
+++ static/js/booking.js
@@ -164,7 +164,4 @@ var requestData = {
 
 
 
-document.getElementById('hamburger').addEventListener('click', function() {
-    var menu = document.getElementById('mobile-menu');
-    menu.classList.toggle('hidden');
-});
+
